69288d6c1dc5ddc69f18e90286431c56f1ae94df
Simyeseol <43331033+Simyeseol@users.noreply.github.com>
2020-07-28 11:24:00 +0900

    ~launch screen
    ( image view, label ) -> stack view
    
    -image view
    add new constraints(256,256 ) ✔️
    
    - stack view
    Add new alignment constraints:
    horizontally,vertically in container✔️

---
+++ 2020MEMO/2020MEMO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2020MEMO
+//
+//  Created by yeseol sim  on 28/07/2020.
+//  Copyright © 2020 yeseol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
